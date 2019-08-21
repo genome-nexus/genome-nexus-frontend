de04993b3485ac6e74cef39f3711424b7d6a3bb3
Xiang Li <xlvv9@mail.missouri.edu>
2019-08-21 17:17:04 -0400

    add transcript table with collapse function

--- src/component/TranscriptSummaryTable.tsx
+++ src/component/TranscriptSummaryTable.tsx
@@ -1,105 +1,147 @@
 import autobind from "autobind-decorator";
 import * as React from 'react';
-import {FormEvent, useState} from "react";
-
-import "./CheckBox.css";
+import "./TranscriptSummaryTable.css";
 import { observer } from "mobx-react";
-import { computed, action } from "mobx";
-import { Form, Button, Row, Col, DropdownButton, Dropdown, Accordion, Card, Collapse, Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { action, observable } from "mobx";
+import { Button, Row, Col, Collapse, Table } from "react-bootstrap";
+import _ from "lodash";
 
 interface ITranscriptSummaryTableProps
 {
+    primaryTranscript: Transcript;
+    otherTranscripts: Transcript[];
+}
 
+type Transcript = {
+    transcript:String,
+    hugoGeneSymbol:string,
+    HGVSShort:string,
+    refSeq:string,
+    variantClassification:string,
+    HGVSC: string,
+    exon: string
 }
 
 @observer
 class TranscriptSummaryTable extends React.Component<ITranscriptSummaryTableProps>
 {
+    @observable showAllTranscript = false;
+
     public render()
     {
         return (
             <div>
                 <Row>
-                    <Col lg="12">
+                    <Col lg="12" className="transcriptTable">
                         <strong>Transcript Consequence Summary</strong>&nbsp;&nbsp;&nbsp;
-                        <TranscriptTable />
+                        <Button
+                            onClick={this.onButtonClick}
+                            aria-controls="table-content"
+                            aria-expanded={this.showAllTranscript}
+                            variant="outline-secondary"
+                            className="btn-sm"
+                        >
+                            see all transcripts
+                        </Button>
+                        {/* make sure we have at least one transcript here*/}
+                        {!this.showAllTranscript && (
+                            TranscriptTable(!this.showAllTranscript, this.props.primaryTranscript)
+                        )}
+                        {this.showAllTranscript && (
+                            TranscriptTable(this.showAllTranscript, this.props.primaryTranscript, this.props.otherTranscripts)
+                        )}
                     </Col>
                 </Row>
             </div>
-
         );
+    }
+
+    @autobind
+    @action
+    onButtonClick() {
+        this.showAllTranscript = !this.showAllTranscript;
     }
 }
 
-
-function TranscriptTable() 
+function TranscriptTable(isOpen:boolean, primaryTranscript:Transcript, otherTranscripts?:Transcript[]) 
 {
-    const [open, setOpen] = useState(false);
-
-    return (
-        <>
-        <Button
-            onClick={() => setOpen(!open)}
-            aria-controls="table-content"
-            aria-expanded={open}
-        >
-            click
-        </Button>
-        <Collapse in={open}>
-            <div id="table-content">
-                <Table responsive striped bordered hover size="sm">
-                    <thead>
-                        <tr>
-                        <th>#</th>
-                        <th>Transcript</th>
-                        <th>Hugo Gene Symbol</th>
-                        <th>HGVS Short</th>
-                        <th>Ref Seq</th>
-                        <th>Variant Classification</th>
-                        <th>HGVSC</th>
-                        <th>Exon</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>1</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        </tr>
-                        <tr>
-                        <td>2</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        </tr>
-                        <tr>
-                        <td>3</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </div>
-        </Collapse>
-        </>
-    );
+    if (_.isEmpty(otherTranscripts)) {
+        return (
+            <>
+            <Collapse in={isOpen}>
+                <div className="tableContent">
+                    <Table responsive striped bordered hover size="sm">
+                        <thead>
+                            <tr>
+                            <th>#</th>
+                            <th>Transcript</th>
+                            <th>Hugo Gene Symbol</th>
+                            <th>HGVS Short</th>
+                            <th>Ref Seq</th>
+                            <th>Variant Classification</th>
+                            <th>HGVSC</th>
+                            <th>Exon</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>1</td>
+                            <td>{primaryTranscript.transcript}</td>
+                            <td>{primaryTranscript.hugoGeneSymbol}</td>
+                            <td>{primaryTranscript.HGVSShort}</td>
+                            <td>{primaryTranscript.refSeq}</td>
+                            <td>{primaryTranscript.variantClassification}</td>
+                            <td>{primaryTranscript.HGVSC}</td>
+                            <td>{primaryTranscript.exon}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+                </Collapse>
+            </>
+        );
+    }
+    else {
+        const allTranscripts = _.concat(otherTranscripts!, primaryTranscript);
+        return (
+            <>
+            <Collapse in={isOpen}>
+                <div className="tableContent">
+                    <Table responsive striped bordered hover size="sm">
+                        <thead>
+                            <tr>
+                            <th>#</th>
+                            <th>Transcript</th>
+                            <th>Hugo Gene Symbol</th>
+                            <th>HGVS Short</th>
+                            <th>Ref Seq</th>
+                            <th>Variant Classification</th>
+                            <th>HGVSC</th>
+                            <th>Exon</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {allTranscripts.map((transcript, index) => {
+                                return (
+                                    <tr>
+                                    <td>{index + 1}</td>
+                                    <td>{transcript.transcript}</td>
+                                    <td>{transcript.hugoGeneSymbol}</td>
+                                    <td>{transcript.HGVSShort}</td>
+                                    <td>{transcript.refSeq}</td>
+                                    <td>{transcript.variantClassification}</td>
+                                    <td>{transcript.HGVSC}</td>
+                                    <td>{transcript.exon}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </Table>
+                </div>
+                </Collapse>
+            </>
+        );
+    }
 }
-
-
 
 export default TranscriptSummaryTable;
--- src/page/VariantStore.ts
+++ src/page/VariantStore.ts
@@ -2,7 +2,7 @@ import { observable } from "mobx";
 
 export class VariantStore {
     @observable public allRecources = ["Cancer Hotspots", "OncoKB", "COSMIC", "cBioPortal",
-                                        "Mutation Assessor", "CIVIC", "PMKB", "SIFT", "Polyphen-2",
+                                        "Mutation Assessor", "CIViC", "PMKB", "SIFT", "Polyphen-2",
                                         "UniProt", "PFAM", "PDB", "ProSite", "PhosphoSitePlus",
                                         "PTM", "External Links"];
     @observable public selectedRecources: string[] = this.allRecources;
