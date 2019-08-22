d57a331f060aaab03b80c72de22ab30d06e3363d
Xiang Li <xlvv9@mail.missouri.edu>
2019-08-22 11:54:29 -0400

    remove tooltip, show hiden text when hover

--- src/component/BasicInfoUnit.tsx
+++ src/component/BasicInfoUnit.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import "./BasicInfoUnit.css";
 import { observer } from "mobx-react";
-import { computed, observable } from "mobx";
-import { Row, Col, OverlayTrigger, Tooltip } from "react-bootstrap";
-import autobind from 'autobind-decorator';
+import { Row, Col } from "react-bootstrap";
 
 interface IBasicInfoUnitProps
 {
@@ -18,58 +16,17 @@ class BasicInfoUnit extends React.Component<IBasicInfoUnitProps>
         data: "N/A"
     };
 
-    @observable tooltipVisible = false;
-    @observable.ref private el:HTMLDivElement|null = null;
-
-    @autobind
-    setRef(el:HTMLDivElement){
-        this.el = el;
-    }
-
-    @computed get isOverflowed() {
-        if (this.el) {
-            let shownWidth =  this.el!.offsetWidth;
-            let actualWidth = this.el!.scrollWidth;
-            
-            return (actualWidth! - shownWidth!) > 1;
-        }
-        return false;
-    }
-
     public render()
     {
         return (
             <Row>
-                <Col lg="4" className="field">
+                <Col lg="4" className="fieldName">
                     {this.props.field}
                 </Col>
-                <Col lg="8">
-                    {this.isOverflowed && (
-                        <OverlayTrigger
-                            key={'top'}
-                            placement={'top'}
-                            overlay={
-                                <Tooltip id={`tooltip-${'top'}`}>
-                                    {this.props.data}
-                                </Tooltip>
-                            }
-                            >
-                            <div ref={this.setRef} className="data" >
-                                {this.props.data}
-                            </div>
-                        </OverlayTrigger>
-                        )
-                    }
-
-                    {!this.isOverflowed && (
-                        <div ref={this.setRef} className="data" >
-                            {this.props.data}
-                        </div>
-                        )
-                    }
+                <Col lg="8" className="data">
+                    {this.props.data}
                 </Col>
             </Row>
-
         );
     }
 }
