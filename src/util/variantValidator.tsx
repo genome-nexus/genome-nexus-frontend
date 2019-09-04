bdc4c27a6ada3698695c132c3ad5d43a3d187410
Xiang Li <xlvv9@mail.missouri.edu>
2019-09-04 09:26:59 -0400

    variantValidator

--- src/util/variantValidator.tsx
+++ src/util/variantValidator.tsx
@@ -1,33 +1,55 @@
-import * as React from 'react';
-import "./BasicInfo.css";
-import { observer } from "mobx-react";
-import { Row, Col } from "react-bootstrap";
-import { VariantAnnotationSummary } from 'cbioportal-frontend-commons';
 import _ from 'lodash';
-import { string } from 'prop-types';
 
-interface IVariantValidatorProps
-{
-    variant: string;
+export enum VALIDATION_MESSAGE {
+    VALID = "This variant is valid",
+    NOT_VALID = "This variant is not valid"
 }
 
-@observer
-class VariantValidator extends React.Component<IVariantValidatorProps>
-{
-    private parseVariant() {
-        const variant = this.props.variant.trim().replace("chr","").replace("23:g.","X:g.").replace("24:g.","Y:g.");
-        const chromosome = variant.split(":g.")[0];
-
-    }
-    public render()
-    {
-        
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+export enum VARIANT_OPERATOR {
+    SNP = ">",
+    INS = "ins",
+    DEL = "del",
+    DELINS = "delins"
 }
 
-export default VariantValidator;
+export type VariantValidStatus = {
+    isValid: boolean,
+    message: VALIDATION_MESSAGE
+}
+
+export function isVariantValid(variant:string):VariantValidStatus {
+    // check if the variant is using genomic location representation
+    // is using genomic location representation
+    if (variant.split(',').length > 1) {
+        // TODO: add check for genomic location
+    }
+    // is using hgvs representation
+    else {
+        // SNP
+        if (variant.includes(VARIANT_OPERATOR.SNP)) {
+            var pattern = /\b([1-9]|1[0-9]|2[0-4])\b(:g.)[0-9]*[A|T|G|C]>[A|T|G|C]/i;
+            if (variant.match(pattern)) {
+                return {"isValid":true, "message":VALIDATION_MESSAGE.VALID} as VariantValidStatus;
+            }
+        }
+        // DELINS
+        else if (variant.includes(VARIANT_OPERATOR.DELINS)) {
+            var pattern = /\b([1-9]|1[0-9]|2[0-4])\b(:g.)[0-9]*_?[0-9]*/i;
+            var pattern = /\b([1-9]|1[0-9]|2[0-4])\b(:g.)[0-9]*[A|T|G|C]>[A|T|G|C]/i;
+            if (variant.match(pattern)) {
+                return {"isValid":true, "message":VALIDATION_MESSAGE.VALID} as VariantValidStatus;
+            }
+
+        }
+        // INS
+        else if (variant.includes(VARIANT_OPERATOR.INS)) {
+
+        }
+        // DEL
+        else if (variant.includes(VARIANT_OPERATOR.DEL)) {
+
+        }
+    }
+    return {"isValid":false, "message":VALIDATION_MESSAGE.NOT_VALID} as VariantValidStatus;
+}
+
