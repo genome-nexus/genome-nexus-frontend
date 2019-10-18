8f856d26b59922c6f10a473ce50b71f54b1222b3
Xiang Li <xlvv9@mail.missouri.edu>
2019-10-18 17:42:48 -0400

    make api url configurable

--- src/page/genomeNexusClientInstance.ts
+++ src/page/genomeNexusClientInstance.ts
@@ -1,5 +1,9 @@
 import { GenomeNexusAPIInternal } from 'cbioportal-frontend-commons';
 
-const client = new GenomeNexusAPIInternal('https://www.genomenexus.org');
+const client =
+    process.env.NODE_ENV === 'production' &&
+    window.location.host === 'localhost:38080'
+        ? new GenomeNexusAPIInternal('http://localhost:38080')
+        : new GenomeNexusAPIInternal('https://www.genomenexus.org');
 
 export default client;
