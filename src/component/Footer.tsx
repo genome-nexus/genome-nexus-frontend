923cb94f218fba74de4dfcb0a2a45ea98cc667b1
Xiang Li <xlvv9@mail.missouri.edu>
2019-12-30 13:31:15 -0500

    fix api link

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -40,7 +40,9 @@ class Footer extends React.Component<{}> {
     public get internalLinks() {
         return (
             <div>
-                <Link to="/swagger-ui.html">API</Link>
+                <a href="/swagger-ui.html" target="_top">
+                    API
+                </a>
                 <a href="https://docs.genomenexus.org" target="_top">
                     Documentation
                 </a>
