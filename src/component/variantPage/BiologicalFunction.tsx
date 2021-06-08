ad667515341118ad04e32efb8bf295848e9ebc06
Xiang Li <xlvv9@mail.missouri.edu>
2021-06-08 11:18:17 -0400

    add clinvar to variant page

--- src/component/variantPage/BiologicalFunction.tsx
+++ src/component/variantPage/BiologicalFunction.tsx
@@ -3,22 +3,28 @@ import { observer } from 'mobx-react';
 
 import Oncokb from './biologicalFunction/Oncokb';
 import { IndicatorQueryResp } from 'oncokb-ts-api-client';
+import ClinvarInterpretation from './biologicalFunction/ClinvarInterpretation';
+import { Clinvar } from 'genome-nexus-ts-api-client';
 
 interface IBiologicalFunctionProps {
     oncokb: IndicatorQueryResp | undefined;
     isCanonicalTranscriptSelected: boolean;
+    clinvar?: Clinvar;
 }
 
 @observer
 class BiologicalFunction extends React.Component<IBiologicalFunctionProps> {
     public render() {
         return (
-            <Oncokb
-                oncokb={this.props.oncokb}
-                isCanonicalTranscriptSelected={
-                    this.props.isCanonicalTranscriptSelected
-                }
-            />
+            <>
+                <Oncokb
+                    oncokb={this.props.oncokb}
+                    isCanonicalTranscriptSelected={
+                        this.props.isCanonicalTranscriptSelected
+                    }
+                />
+                <ClinvarInterpretation clinvar={this.props.clinvar} />
+            </>
         );
     }
 }
